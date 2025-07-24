aef732520c27b5ae941171b9d7cfc22a69a9131c
garesuta <39191258+garesuta@users.noreply.github.com>
2025-07-24 23:39:10 +0700

    Step 1: project setup

---
+++ server/eslint.config.mjs
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import drizzlePlugin from "eslint-plugin-drizzle";
+import eslintPrettierConfig from "eslint-config-prettier";
+import {fixupPluginRules} from "@eslint/compat";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  {
+    plugins: {
+      drizzle: fixupPluginRules(drizzlePlugin),
+    },
+  }
+]);
