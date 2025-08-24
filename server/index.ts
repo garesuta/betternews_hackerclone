1b3550b71b5a4a37f90841810bdab0b4b43b34e6
garesuta <39191258+garesuta@users.noreply.github.com>
2025-08-24 13:54:23 +0700

    Step 3: hono error handling

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,6 @@
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
+import type { ErrorResponse } from "../shared/types";
 
 const app = new Hono();
 
@@ -6,6 +8,20 @@ app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
-export default app;
+app.onError((err, c) => {
+  if(err instanceof HTTPException){
+    const errResponse = err.res ?? c.json<ErrorResponse>({
+      success:false,
+      error: err.message,
+      isFormError: err.cause && typeof err.cause === "object" && "form" in err.cause ? err.cause.form == true: false,
+    });
+    return errResponse;
+  }
+  return c.json<ErrorResponse>({
+    success: false,
+    error: process.env.NODE_ENV === "production" ? "Internal Server Error" : (err.stack ?? err.message),
+  },500
+);
+});
 
-// 21:53 eslint
+export default app;
